890c148f4e0c074ed5f52d62967e7c352f6f5a77
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-06-03 02:49:17 +0530

    added setTimeout function

--- commands/ping.js
+++ commands/ping.js
@@ -1,8 +1,10 @@
 module.exports = {
     callback: (message) => {
-        message.channel.send('scald is loading').then (async (msg) =>{
-            msg.delete()
-            message.channel.send(`ping is ${msg.createdTimestamp - message.createdTimestamp}ms.`)
+        message.channel.send('scald is loading...').then (async (msg) =>{
+            setTimeout(function(){
+              msg.delete()
+              message.channel.send(`ping is ${msg.createdTimestamp - message.createdTimestamp}ms.`)
+            }, 1000 * 2)
         })
     }
 }
