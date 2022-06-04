52addde1efe9a86c28dc9b9deef3024a5c8eaeef
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-06-04 08:18:29 +0530

    added a condition if a key already exists

--- commands/addDb.js
+++ commands/addDb.js
@@ -7,6 +7,28 @@ module.exports = {
         var str = arr.join(' ')
         if (argument === '')
         message.reply('add something')
+        else if (db.has(argument) == true) {
+            let filter = m => m.author.id === message.author.id
+            message.channel.send("a key with the same name already exists, type `yes` to overwrite it").then(() => {
+                message.channel.awaitMessages(filter, {
+                    max: 1,
+                    time: 30000,
+                    errors: ['time']
+                  })
+                  .then(message => {
+                      message = message.first()
+                      if (message.content.toUpperCase() == 'YES' || message.content.toUpperCase() == 'Y') {
+                          db.set(argument, str)
+                          message.reply('done')
+                      } else {
+                      message.channel.send(`procedure cancelled`)
+                  }
+              })
+              .catch(collected => {
+                  message.channel.send('Timeout');
+              });
+          })
+        }
         else {
           db.set(argument, str)
           message.reply('done');
