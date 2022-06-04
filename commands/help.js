b009da487cb63b33cf55ac294d645116c0305fc4
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-06-04 06:33:44 +0530

    bruh

--- commands/help.js
+++ commands/help.js
@@ -1,5 +1,5 @@
 module.exports = {
     callback: (message) => {
-        message.channel.send('```[ALL COMMANDS] \n \n {addDb} - syntax:  !scald <key> <value>     // <key> should be a single word or multpile words joint with special characters like underscores \n {cp} - syntax: !scald cp <key>              //prints the value related to the key in the db \n {echo} - syntax: !scald echo <value>        //repeats your message \n {hi} - syntax: !scald hi                    //wtf was wrong with me when i made that \n {ping} - syntax: !scald ping                //pong \n \n *gonna add more shit later*')
+        message.channel.send('```[ALL COMMANDS] \n \n {addDb} - syntax:  !scald <key> <value>     // <key> should be a single word or multpile words joint with special characters like underscores \n {cp} - syntax: !scald cp <key>              //prints the value related to the key in the db \n {echo} - syntax: !scald echo <value>        //repeats your message \n {hi} - syntax: !scald hi                    //wtf was wrong with me when i made that \n {ping} - syntax: !scald ping                //pong \n \n *gonna add more shit later```')
     }
 }
