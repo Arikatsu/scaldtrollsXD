3fc88127e2e54c4fe07f45c44464c7b0d5e03721
Shichiha <77842398+Shichiha@users.noreply.github.com>
2022-06-06 01:53:24 +0900

    patch

--- commands/echo.js
+++ commands/echo.js
@@ -3,15 +3,9 @@ const bl = require('../blacklist.json')
 module.exports = {
     callback: (message, ...args) => {
         var string = args.join(" ")
-        let checker = false;
         if (new RegExp(bl.join("|")).test(string)) {
-            checker = true;
-        }
-        if (checker == true)
-        message.reply("your message contains blacklisted words")
+            message.reply("your message contains blacklisted words")}
         else {
-            message.channel.send(string)
-        }
-        checker = false;
+            message.channel.send(string)}
     }
 }
