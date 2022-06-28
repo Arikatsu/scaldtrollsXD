d2dd60be75487f4082ff0e2cf2dc949d550a5e62
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-06-28 09:32:15 +0530

    sex

--- commands/sex.js
+++ commands/sex.js
@@ -6,36 +6,59 @@ const { MessageAttachment } = require('discord.js')
 module.exports = {
     callback: async (message, ...args) => {
         const msg = await message.channel.send('working on it...')
-       try {
-        let member1 = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args[0].toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName === args[0].toLocaleLowerCase());
-        let member2 = message.mentions.members.first(2)[1] || message.guild.members.cache.get(args[1]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args[1].toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName === args[1].toLocaleLowerCase());
+        try {
+            let member1 = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args[0].toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName === args[0].toLocaleLowerCase());
 
-        const canvas = Canvas.createCanvas(750, 1060);
-        const context = canvas.getContext('2d');
+            if (!args[1]) {
+                const canvas = Canvas.createCanvas(471, 651);
+                const context = canvas.getContext('2d');
 
-        const backgroundFile = await readFile('./db/hentai.jpg');
-        const background = new Canvas.Image();
-        background.src = backgroundFile;
+                const backgroundFile = await readFile('./db/hutao.jpeg');
+                const background = new Canvas.Image();
+                background.src = backgroundFile;
 
-        context.drawImage(background, 0, 0, canvas.width, canvas.height);
+                context.drawImage(background, 0, 0, canvas.width, canvas.height);
 
-        const body1 = (await request(member1.displayAvatarURL({ format: 'jpg' })))["body"];
-        const avatar1 = new Canvas.Image();
-        avatar1.src = Buffer.from(await body1.arrayBuffer());
+                const { body } = await request(member1.displayAvatarURL({ format: 'jpg' }));
+                const avatar = new Canvas.Image();
+                avatar.src = Buffer.from(await body.arrayBuffer());
+                
+                context.drawImage(avatar, 143, 60, 162, 162);
 
-        const body2 = (await request(member2.displayAvatarURL({ format: 'jpg' })))["body"];
-        const avatar2 = new Canvas.Image();
-        avatar2.src = Buffer.from(await body2.arrayBuffer());
+                const attachment = new MessageAttachment(canvas.toBuffer('image/png'), 'hentai.png');
 
-        context.drawImage(avatar1, 128, 244, 150, 150);
-        context.drawImage(avatar2, 296, 189, 150, 150);
+                msg.edit({ content: "done", files: [attachment] })
 
-        const attachment = new MessageAttachment(canvas.toBuffer('image/png'), 'hentai.png');
-        
-        msg.edit({ content: "done", files: [attachment] })
-       } catch(err) {
-        msg.delete()
-        message.channel.send('Error, check your inputs').then(console.log(err))
-       }
-    }   
+            } else if (!args[2]) {
+                let member2 = message.mentions.members.first(2)[1] || message.guild.members.cache.get(args[1]) || message.guild.members.cache.find(r => r.user.username.toLowerCase() === args[1].toLocaleLowerCase()) || message.guild.members.cache.find(r => r.displayName === args[1].toLocaleLowerCase());
+
+                const canvas = Canvas.createCanvas(750, 1060);
+                const context = canvas.getContext('2d');
+
+                const backgroundFile = await readFile('./db/eimiko.jpg');
+                const background = new Canvas.Image();
+                background.src = backgroundFile;
+
+                context.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+                const body1 = (await request(member1.displayAvatarURL({ format: 'jpg' })))["body"];
+                const avatar1 = new Canvas.Image();
+                avatar1.src = Buffer.from(await body1.arrayBuffer());
+
+                const body2 = (await request(member2.displayAvatarURL({ format: 'jpg' })))["body"];
+                const avatar2 = new Canvas.Image();
+                avatar2.src = Buffer.from(await body2.arrayBuffer());
+
+                context.drawImage(avatar1, 81, 171, 207, 207);
+                context.drawImage(avatar2, 295, 122, 207, 207);
+
+                const attachment = new MessageAttachment(canvas.toBuffer('image/png'), 'hentai.png');
+
+                msg.edit({ content: "done", files: [attachment] })
+            }
+        } catch (err) {
+            msg.delete()
+            message.channel.send('Error, check your inputs').then(console.log(err))
+        }
+    }
 }
