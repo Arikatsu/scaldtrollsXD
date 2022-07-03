b56a986d7c5809805e33f33eb7418a3f27506b6b
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-07-03 06:16:30 +0530

    bruh

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,6 +1,9 @@
+const replies = ['no', 'nah', 'fuck no', 'git gud', 'lol no', 'shut the fuck up', 'never', 'ur dumb lol', 'yes', 'yeah', 'ok', 'sure', 'of course idiot', 'yes imo', 'ong bro', 'okay']
+
 module.exports = {
     callback: (message, ...args) => {
-        const replies = ['no', 'nah', 'fuck no', 'git gud', 'lol no', 'shut the fuck up', 'never', 'ur dumb lol', 'yes', 'yeah', 'ok', 'sure', 'of course idiot', 'yes imo', 'ong bro', 'okay']
+       if (!args[0]) return
+       else {
         if (args[0].toLowerCase() == 'pick-') {
             args = args.slice(1)
             let choice = args[Math.floor(Math.random() * args.length)];
@@ -10,5 +13,6 @@ module.exports = {
             let reply = replies[Math.floor(Math.random() * replies.length)];
             message.reply(reply)
         }
+       }
     }
 }
