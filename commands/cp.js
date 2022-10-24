9e6d6e9ca0088586f17f2217dcd99c8ed654b80f
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-10-25 00:24:32 +0530

    improve copypasta command

--- commands/cp.js
+++ commands/cp.js
@@ -2,41 +2,48 @@ const chaos = require('../db/chaos.json')
 const fs = require('fs')
 
 module.exports = {
-    callback: (message, ...args) => {
-        var argument = args[0].toLowerCase()
-        var arr = args.slice(1);
-        var str = arr.join(' ')
+    callback: (message, ...args) => {        
+        let checker = args.indexOf('|')
+        if (checker === -1) 
+            return message.reply('Use the correct format jesse: `?cp <key> | <value>`')
+
+        let keyArray = args.slice(0, checker)
+        let valueArray = args.slice(checker + 1)
+
+        let argument = keyArray.join(' ')
+        let str = valueArray.join(' ')
+
         let json = null
+
         try {
             json = JSON.parse(chaos) 
         } catch (e) {
             json = chaos;
         }
+
         if (!argument)
             message.reply('add something')
         else if (chaos[argument]) {
             let msgfilter = m => m.author.id === message.author.id
-            message.channel.send("a key with the same name already exists, type `yes` to overwrite it").then(() => {
+            message.channel.send("A key with the same name already exists, type `yes` to overwrite it").then(() => {
                 message.channel.awaitMessages({
                     filter: msgfilter,
                     max: 1,
                     time: 30000,
                     errors: ['time']
-                })
-                    .then(message => {
-                        message = message.first()
-                        if (message.content.toUpperCase() == 'YES' || message.content.toUpperCase() == 'Y') {
-                            json[argument] = str
-                            let data = JSON.stringify(json, null, 2)
-                            fs.writeFileSync(`./db/chaos.json`, data)
-                            return message.reply('done')
-                        } else {
-                            return message.channel.send(`procedure cancelled`)
-                        }
-                    })
-                    .catch(collected => {
-                        message.channel.send('Timeout');
-                    });
+                }).then(message => {
+                    message = message.first()
+                    if (message.content.toUpperCase() == 'YES' || message.content.toUpperCase() == 'Y') {
+                        json[argument] = str
+                        let data = JSON.stringify(json, null, 2)
+                        fs.writeFileSync(`./db/chaos.json`, data)
+                        return message.reply('done')
+                    } else {
+                        return message.channel.send(`procedure cancelled`)
+                    }
+                }).catch(collected => {
+                    message.channel.send('Timeout');
+                });
             })
         }
         else {
